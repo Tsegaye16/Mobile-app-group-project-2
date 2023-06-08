209dd6802d96533f7285352cf4fa5602a9257e66
kefleaseres1 <kefleaseres29@gmail.com>
2023-06-08 11:45:07 +0300

    drawer

---
+++ practice/lib/widgets/drawer.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../Screens/about_Screen.dart';
+import '../Screens/developer_Screen.dart';
+import '../main.dart';
+
+class DrawerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Yejoka Restaurant',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Best food in town!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('About'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AboutPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Developer'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DeveloperPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
